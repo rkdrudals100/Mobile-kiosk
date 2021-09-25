5ba2d984b929c37660f9fb6501399f903723368a
Sungho_Park <tjdgh1785@gmail.com>
2021-09-25 19:47:07 +0900

    09/25 jg -> sungho 병합(매핑 수정해야함)

--- src/main/java/com/graduate/mobilekiosk/repository/CategoryRepository.java
+++ src/main/java/com/graduate/mobilekiosk/repository/CategoryRepository.java
@@ -1,9 +1,16 @@
 package com.graduate.mobilekiosk.repository;
 
 import com.graduate.mobilekiosk.domain.Category;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    @EntityGraph(attributePaths = {"items"})
+    public List<Category> findByUserName(String name);
+
+    public Category findByNameAndUserName(String categoryName, String userName);
 }
--- src/main/java/com/graduate/mobilekiosk/web/controller/MenuController.java
+++ src/main/java/com/graduate/mobilekiosk/web/controller/MenuController.java
@@ -1,28 +1,73 @@
 package com.graduate.mobilekiosk.web.controller;
 
+import com.graduate.mobilekiosk.domain.Category;
+import com.graduate.mobilekiosk.domain.Item;
+import com.graduate.mobilekiosk.domain.Member;
+import com.graduate.mobilekiosk.repository.CategoryRepository;
+import com.graduate.mobilekiosk.repository.ItemRepository;
+import com.graduate.mobilekiosk.repository.MemberRepository;
+import com.graduate.mobilekiosk.web.dto.CategoryDto;
+import com.graduate.mobilekiosk.web.dto.MenuSaveDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
+@Slf4j
 @RequestMapping
+@RequiredArgsConstructor
 public class MenuController {
 
-    @GetMapping("/menu-management")
-    public String moveMenu(Model model, Principal principal) {
-        model.addAttribute("username", principal.getName());
-        return "seller/menu-management.html";
+    private final MemberRepository memberRepository;
+    private final CategoryRepository categoryRepository;
+    private final ItemRepository itemRepository;
 
+    @GetMapping("/menu-management")
+    public String menu(Model model, Principal principal) {
+        List<Category> categories = categoryRepository.findByUserName(principal.getName());
+
+        model.addAttribute("username", principal.getName());
+        model.addAttribute("categories", categories);
+        return "seller/menu-management.html";
     }
 
-    @GetMapping("/order-management")
-    public String moveOrder(Model model, Principal principal) {
-        model.addAttribute("username", principal.getName());
-        return "seller/order-management.html";
+    @PostMapping("")
+    public String categoryAdd(@Validated @ModelAttribute CategoryDto categoryDto, BindingResult bindingResult, Principal principal) {
+        if (bindingResult.hasErrors()) {
+            return "redirect:/menus";
+        }
 
+        Member member = memberRepository.findByUserId(principal.getName());
+
+        Category category = Category.builder()
+                .member(member)
+                .name(categoryDto.getCategoryName())
+                .userName(principal.getName())
+                .build();
+
+        categoryRepository.save(category);
+
+        return "redirect:/menus";
+    }
+
+    @PostMapping("add")
+    public String menuAdd(@ModelAttribute MenuSaveDto menuSaveDto, Principal principal) {
+        Category findCategory = categoryRepository.findByNameAndUserName(menuSaveDto.getCategoryName(), principal.getName());
+        Item item = Item.builder()
+                .category(findCategory)
+                .name(menuSaveDto.getMenuName())
+                .description(menuSaveDto.getDescription())
+                .build();
+
+        itemRepository.save(item);
+        return "redirect:/menus";
     }
 
     @GetMapping("/qr")
@@ -30,5 +75,11 @@ public class MenuController {
         model.addAttribute("username", principal.getName());
         return "seller/qr.html";
 
+    }
+
+    @GetMapping("/order-management")
+    public String moveOrder(Model model, Principal principal) {
+        model.addAttribute("username", principal.getName());
+        return "seller/order-management.html";
     }
 }
