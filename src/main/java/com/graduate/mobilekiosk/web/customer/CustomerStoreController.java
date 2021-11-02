d997e97d74ba6cf2095eab7bb3c6d70a8607e558
rkdrudals100 <74872542+rkdrudals100@users.noreply.github.com>
2021-11-02 15:53:16 +0900

    customer-alert 에서 추가 주문 클릭 시 모달이 뜨도록 수정

--- src/main/java/com/graduate/mobilekiosk/web/customer/CustomerStoreController.java
+++ src/main/java/com/graduate/mobilekiosk/web/customer/CustomerStoreController.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
